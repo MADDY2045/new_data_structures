9b8ede825bd20c77602d78b12104a944d7fb9c8d
MADDY2045 <madhavaneee08@gmail.com>
2024-10-16 16:53:24 +0530

    priority queues

--- index.js
+++ index.js
@@ -124,6 +124,85 @@ class MaxBinaryHeap {
   }
 }
 
+/**
+ * Priority queue
+ * Write a min binary heap - lower number means higher priority
+ * Each node has a val and a priority. Use the priority to build the heap
+ * Enqueue method accepts a value and priority, makes a new node, and
+ * puts it in the right spot based of its priority
+ * Dequeue method removes root element, returns it, and rearranges
+ * heap using priority
+ */
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 console.log('********CREATING HEAP*******');
 let heap = new MaxBinaryHeap([41, 39, 33, 18, 27, 12]);
 console.log('heap:::', heap);
@@ -134,3 +213,14 @@ console.log('heap after insert:::', heap);
 console.log('********EXTRACT*******');
 let output = heap.extractMax();
 console.log('extracted value:', output);
+
+/******************* PRIORITY QUEUE *************************/
+let ER = new PriorityQueue();
+ER.enqueue('Concussion', 3);
+ER.enqueue('Common Cold', 5);
+ER.enqueue('Gunshot', 1);
+
+ER.enqueue('Labor Pain', 0);
+let dequedPriority = ER.dequeue();
+console.log('dequedPriority:', dequedPriority);
+console.log(ER);
