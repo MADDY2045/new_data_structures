65377f7702e063854aea937b075101de8e1f2852
MADDY2045 <madhavaneee08@gmail.com>
2024-10-16 16:30:21 +0530

    binary heaps basics

--- index.js
+++ index.js
@@ -1,250 +1,136 @@
-//Node for tree
-class Node {
-  constructor(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+/**
+ * Binary heaps are fascinating data structures, mainly used to implement priority queues, and they come in two flavors:
+ * Min-Heaps and Max-Heaps.
+ *
+ *
+ * Min-Heap:
+ * ---------
+ * Property:
+ * In a min-heap, the value of each node is greater than or equal to the value of its parent.
+ * The smallest element is at the root.
+ * Operations:
+ * ------------
+ * Insertion: Add the new element at the end of the heap, then "bubble up" to maintain heap property.
+ * Deletion (Extract Min): Remove the root, replace it with the last element, then "bubble down" to maintain heap property.
+ *
+ *
+ * Max-Heap
+ * --------
+ * Property:
+ * In a max-heap, the value of each node is less than or equal to the value of its parent.
+ * The largest element is at the root.
+ * Operations:
+ * -----------
+ * Insertion: Add the new element at the end, then "bubble up".
+ * Deletion (Extract Max): Remove the root, replace with the last element, then "bubble down".
+ *
+ * Structure
+ * ----------
+ * Array Representation: Binary heaps can be efficiently stored in arrays. For any given index i:
+ * The parent node is at index (i-1) // 2.
+ * The left child is at index 2*i + 1.
+ * The right child is at index 2*i + 2.
+ */
+
+class MaxBinaryHeap {
+  constructor(args) {
+    this.values = [...args];
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    let values = this.values;
+    while (idx > 0) {
+      //parentIndex
+      let parentIndex = Math.floor((idx - 1) / 2);
+      let parent = values[parentIndex];
+      //compare the curretn item with parentItem
+      //Using indices calculated
+      if (element <= parent) break; //base
+      //otherwise
+      //swap the values
+      values[parentIndex] = element;
+      values[idx] = parent;
+      //now update the index to be parentidx
+      idx = parentIndex;
+    }
+  }
+
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+
+  /**
+   * Swap the first value in the values property with the last
+   * Pop from the values property, so we can return at end
+   * Have the new root "sink down" to correct spot
+   *  Your parent index starts at 0
+   *  Find the index of the left child: 2 * index + 1(should not be out of bound)
+   *  Find the index of the right child: 2 * index + 2(should not be out of bound)
+   *  If the left or right child is greater than the element..swap.
+   *  If both left and right children are larger, swap with the largest child
+   *  The child index you swapped becomes the new parent index
+   *  keep looping and swapping until neither child is larger than element
+   *  Return the old root
+   */
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * idx + 1;
+      let rightChildIndex = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+      /**
+       * Handle scenarios when both left index
+       * and right index are larger than element
+       * and right index is largest
+       */
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0]; //max value
+    const end = this.values.pop(); //extract this last variable to sink down
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      //sinkDown
+      this.sinkDown();
+    }
+    return max;
   }
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-
-  /**
-   * Insert
-   * Create a new node
-   * Starting at the root,
-   *  Check if there is a root, if not, the root now becomes that new node
-   *  if there is a root,
-   *    check if the value of the new node >= value of the root
-   *      if it's greater,
-   *        Check to see if there is a node to right
-   *          if yes, move to that node and repeat the steps
-   *          if not, add that node as right property
-   *      if it's less
-   *         Check to see if there is a node to the left
-   *           if yes, move to that node and repeat these steps
-   *           if not, add that node as the left property
-   */
-
-  insert(value) {
-    let newNode = new Node(value);
-    //check if there is not root
-    if (this.root === null) {
-      //assign the value as new root
-      this.root = newNode;
-      return this;
-    } else {
-      let current = this.root;
-      while (true) {
-        //as we dont have any parameter to control
-        if (value === current.value) return this;
-        /* check for the left side portions */
-        if (value < current.value) {
-          if (current.left === null) {
-            //There s nothing on the left
-            current.left = newNode;
-            return this;
-          } else {
-            //repeat by reassigning the current left
-            current = current.left;
-          }
-        }
-        /* check for the right side portions */
-        if (value > current.value) {
-          if (current.right === null) {
-            //There s nothing on the left
-            current.right = newNode;
-            return this;
-          } else {
-            //repeat by reassigning the current right
-            current = current.right;
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * Starting at the root
-   * If there is not root, we are done searching
-   * If there s a root, check if value is equal to
-   * the root, and if yes return it
-   * Initialise a current root(this.root) and found(boolean) variable
-   * If not,
-   *  Check if value > value of root
-   *    If Yes
-   *      Check if there is a node to right
-   *        if yes
-   *           move to that node and repeat the process
-   *        if no
-   *            if not, we are done searching
-   *    If No
-   *       Check if there is a node to left
-   *        if yes
-   *           move to that node and repeat the process
-   *        if no
-   *            if not, we are done searching
-   */
-  find(value) {
-    if (this.root === null) return undefined;
-    let current = this.root;
-    let found = false;
-    while (current && !found) {
-      //left side search
-      if (value < current.value) {
-        current = current.left;
-      }
-      //right side search
-      else if (value > current.value) {
-        current = current.right;
-      } else {
-        found = true;
-      }
-    }
-    if (!found) return undefined;
-    return current;
-  }
-
-  /**
-   * Explanation:
-   * -------------
-   * Initialize Queue: Start with an empty queue and enqueue the root node.
-   * While Loop: Continue the loop until the queue is empty.
-   * Dequeue Node: Remove the front node from the queue and process it (e.g., print its value).
-   * Enqueue Children: Add all the child nodes (left and right) of the current node to the queue.
-   * Repeat
-   */
-  BFS() {
-    let queue = [];
-    let visitedNodes = [];
-    let node = this.root;
-    //Push the root node into the queue for the first time
-    queue.push(node);
-    while (queue.length) {
-      node = queue.shift();
-      visitedNodes.push(node.value);
-      if (node.left) queue.push(node.left); //connecting statement for while
-      if (node.right !== null) queue.push(node.right); //connecting statement for while
-    }
-    return visitedNodes;
-  }
-
-  /**
-   * DFS
-   * Create a variable to store the values of nodes visited
-   * Store the root of the BST in a variable
-   * Write a helper function which accepts the value
-   *    push the value of the node to the variable that stores the values
-   *    if the node has a left property,
-   *        call the helper with left property on the node
-   *    if the node has a right property,
-   *        call the helper with right property on the node
-   * Invoke the helper function with the current variable
-   * Return the array
-   */
-  DfsPreOrder() {
-    let current = this.root;
-    let data = [];
-
-    function traverseDFS(node) {
-      data.push(node.value);
-      if (node.left) {
-        traverseDFS(node.left);
-      }
-      if (node.right) {
-        traverseDFS(node.right);
-      }
-    }
-    traverseDFS(current); //main recursive starts here
-    return data;
-  }
-
-  /**
-   * DFS
-   * Create a variable to store the values of nodes visited
-   * Store the root of the BST in a variable
-   * Write a helper function which accepts the value
-   *    if the node has a left property,
-   *        call the helper with left property on the node
-   *    if the node has a right property,
-   *        call the helper with right property on the node
-   *    push the value of the node to the variable that stores the values
-   * Invoke the helper function with the current variable
-   * Return the array
-   */
-  DfsPostOrder() {
-    let current = this.root;
-    let data = [];
-
-    function traverseDFS(node) {
-      if (node.left) {
-        traverseDFS(node.left);
-      }
-      if (node.right) {
-        traverseDFS(node.right);
-      }
-      data.push(node.value); //this is the only diff between preorder
-    }
-    traverseDFS(current); //main recursive starts here
-    return data;
-  }
-
-  /**
-   * DFS
-   * Create a variable to store the values of nodes visited
-   * Store the root of the BST in a variable
-   * Write a helper function which accepts the value
-   *    if the node has a left property,
-   *        call the helper with left property on the node
-   *    push the value of the node to the variable that stores the values
-   *    if the node has a right property,
-   *        call the helper with right property on the node
-   * Invoke the helper function with the current variable
-   * Return the array
-   */
-  DfsInOrder() {
-    let current = this.root;
-    let data = [];
-
-    function traverseDFS(node) {
-      if (node.left) {
-        traverseDFS(node.left);
-      }
-      data.push(node.value); //this is the only diff
-      if (node.right) {
-        traverseDFS(node.right);
-      }
-    }
-    traverseDFS(current); //main recursive starts here
-    return data;
-  }
-}
-
-//      10
-//    6     15
-//  3   8     20
-
-console.log('********** INSERT ***********');
-let bst = new BinarySearchTree();
-bst.insert(10);
-bst.insert(6);
-bst.insert(15);
-bst.insert(3);
-bst.insert(8);
-bst.insert(20);
-console.log(bst);
-console.log('********** BFS ***********');
-let data = bst.BFS();
-console.log(data);
-console.log('********** DFS- preOrder ***********');
-let preResponse = bst.DfsPreOrder();
-console.log(preResponse);
-console.log('********** BFS - postOrder***********');
-let postResponse = bst.DfsPostOrder();
-console.log(postResponse);
-console.log('********** BFS - inOrder***********');
-let inOrderResponse = bst.DfsInOrder();
-console.log(inOrderResponse);
+console.log('********CREATING HEAP*******');
+let heap = new MaxBinaryHeap([41, 39, 33, 18, 27, 12]);
+console.log('heap:::', heap);
+console.log('********INSERT*******');
+heap.insert(55);
+heap.insert(1);
+console.log('heap after insert:::', heap);
+console.log('********EXTRACT*******');
+let output = heap.extractMax();
+console.log('extracted value:', output);
